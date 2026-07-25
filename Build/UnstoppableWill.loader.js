17faa1c2aa9af887ca45f78d493a0bbc3d709bfa
DandyTop <33599918+DandyTop@users.noreply.github.com>
2026-07-25 22:24:33 +0700

    test_CameraSize_2224_250726

--- Build/UnstoppableWill.loader.js
+++ Build/UnstoppableWill.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 55742937,
+    wasmFileSize: 55743556,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
