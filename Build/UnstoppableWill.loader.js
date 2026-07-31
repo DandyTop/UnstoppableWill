2a3953f33ace80e8bfda8f7a7ae03cfd266e210d
DandyTop <33599918+DandyTop@users.noreply.github.com>
2026-07-31 17:28:47 +0700

    testSortByY_1728_310726

--- Build/UnstoppableWill.loader.js
+++ Build/UnstoppableWill.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 55743556,
+    wasmFileSize: 55761594,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
